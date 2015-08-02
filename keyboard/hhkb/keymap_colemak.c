b9e8ceadce8aa258fca84647dcaa0fc0c3e9bdc0
Callum Oakley <c.oakley108@gmail.com>
2015-08-02 12:06:02 +0100

    removed toggle, moved grave and tilde

--- keyboard/hhkb/keymap_colemak.c
+++ keyboard/hhkb/keymap_colemak.c
@@ -1,13 +1,13 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    KEYMAP(FN0,    1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSLS, DEL, \
+    KEYMAP(GRV,    1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSLS, DEL, \
            TAB,    Q,   W,   F,   P,   G,   J,   L,   U,   Y,SCLN,LBRC,RBRC,BSPC,      \
            LGUI,   A,   R,   S,   T,   D,   H,   N,   E,   I,   O,QUOT, ENT,           \
            LSFT,   Z,   X,   C,   V,   B,   K,   M,COMM, DOT,SLSH,RSFT, ESC,           \
-                LALT,LCTL,           FN1,                RCTL,RALT),
+                LALT,LCTL,           FN0,                RCTL,RALT),
 
-    KEYMAP(TRNS,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, GRV,TRNS, \
+    KEYMAP(TRNS,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,TRNS,TRNS, \
            TRNS, PWR,TRNS,TRNS,PSCR,CAPS,HOME,PGDN,PGUP, END,MUTE,VOLD,VOLU,TRNS,      \
            TRNS, F15, F16, F17, F18, F19,LEFT,DOWN,  UP,RGHT, F13, F14,TRNS,           \
            TRNS,TRNS,TRNS,TRNS,TRNS, SPC, SPC,MPLY,MPRV,MNXT,PAUS,TRNS,TRNS,           \
@@ -15,6 +15,5 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_TOGGLE(1),
-    [1] = ACTION_LAYER_TAP_KEY(1, KC_SPACE),
+    [0] = ACTION_LAYER_TAP_KEY(1, KC_SPACE)
 };
