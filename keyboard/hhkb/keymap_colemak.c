e0c6d6fe682d59f191963f4024cd61cb1c73fb4d
Callum Oakley <c.oakley108@gmail.com>
2016-05-10 23:20:18 +0100

    made layout more planck friendly

--- keyboard/hhkb/keymap_colemak.c
+++ keyboard/hhkb/keymap_colemak.c
@@ -1,17 +1,22 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    KEYMAP(ESC,    1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS, EQL,BSLS, GRV, \
-           TAB,    Q,   W,   F,   P,   G,   J,   L,   U,   Y,SCLN,LBRC,RBRC, DEL,      \
+    KEYMAP(F12,   F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11,MUTE,VOLD,VOLU, \
+           TAB,    Q,   W,   F,   P,   G,   J,   L,   U,   Y,SCLN, DEL,CAPS, FN4,      \
            BSPC,   A,   R,   S,   T,   D,   H,   N,   E,   I,   O,QUOT, ENT,           \
            FN1,    Z,   X,   C,   V,   B,   K,   M,COMM, DOT,SLSH, FN2, FN3,           \
                 LALT,LCTL,           FN0,                LGUI,RALT),
 
+    KEYMAP(F12,   F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11,PSCR,PAUS,TRNS, \
+           GRV,    1,   2,   3,   4,   5,   6,   7,   8,   9,   0,MINS,TRNS,TRNS,      \
+           ESC, LBRC,LEFT,  UP,DOWN,RGHT,LEFT,DOWN,  UP,RGHT,RBRC, EQL,TRNS,           \
+           TRNS, SPC,HOME,PGUP,PGDN, END,HOME,PGDN,PGUP, END,BSLS,TRNS,TRNS,           \
+                TRNS,TRNS,          TRNS,               TRNS,TRNS),
 
-    KEYMAP(TRNS,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12,PSCR,PAUS, \
-           TRNS, PWR,HOME,PGUP,PGDN, END,HOME,PGDN,PGUP, END,MUTE,VOLD,VOLU,TRNS,      \
-           TRNS,CAPS,LEFT,  UP,DOWN,RGHT,LEFT,DOWN,  UP,RGHT, FN4,TRNS,TRNS,           \
-           TRNS,TRNS,TRNS,TRNS,TRNS, SPC, SPC,MPLY,MPRV,MNXT,MSTP,TRNS,TRNS,           \
+    KEYMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MPLY,MPRV,MNXT,TRNS, \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,TRNS,      \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,           \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PAUS,           \
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),
 };
 
@@ -20,5 +25,5 @@ const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_MODS_ONESHOT(MOD_LSFT),
     [2] = ACTION_MODS_ONESHOT(MOD_RSFT),
     [3] = ACTION_MODS_KEY(MOD_LGUI, KC_GRV),
-    [4] = ACTION_MODS_KEY(MOD_LALT, KC_TAB)
+    [4] = ACTION_LAYER_MOMENTARY(2)
 };
